1439c08e3f349e61f687733bf4ad75889be4da5e
Kpontius3 <kpontius3@gatech.edu>
2025-01-31 21:21:01 -0500

    Chapter 4

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+#Movies is a list of dictionaries, every movie in movies should have:
+#an integer id, name, price, and description at minimum
+
+movies = []
+
+#Renders the movies/index.html template
+def index(request):
+    templateData = {}
+    templateData['title'] = 'Movies'
+    templateData['movies'] = movies
+    return render(request, 'movies/index.html', {'template_data': templateData})
+
+#Grab movie with associated id from list, pass
+#that movie's name and id to movies/show.html template
+def show(request, id):
+    movie = movies[id - 1]
+    templateData = {}
+    templateData['title'] = movie['name']
+    templateData['movie'] = movie
+    return render(request, 'movies/show.html', {'template_data': templateData})
