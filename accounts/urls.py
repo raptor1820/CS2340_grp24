2dce063d2f0b9695a26ef1e1045c76212d8858e7
Ritwic <ritwic.verma.sachan@gmail.com>
2025-01-31 20:14:21 -0500

    implemented user registration, login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
+    path("signup/", views.register, name="signup"),
     path("login/", views.log_in, name="login"),
+    path("logout/", views.log_out, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render
-# Create your views here.
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as  user_login, logout as user_logout, authenticate
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
-def register(request):
-    return render(request,"register.html")
+def signup(request):
+    template_data = {}
+    template_data['title'] = 'Sign Up'
+    if request.method == 'GET':
+        template_data['form'] = CustomUserCreationForm()
+        return render(request, 'accounts/signup.html',
+            {'template_data': template_data})
+    elif request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home.index')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html',
+                {'template_data': template_data})
 
-def log_in(request):
-    return render(request, "login.html")
-
-
-
+def login(request):
+    template_data = {}
+    template_data['title'] = 'Login'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html',
+            {'template_data': template_data})
+    elif request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user is None:
+            template_data['error'] ='The username or password is incorrect.'
+            return render(request, 'accounts/login.html',
+                {'template_data': template_data})
+        else:
+            user_login(request, user)
+            return redirect('home.index')
+        
+@login_required
+def logout(request):
+    user_logout(request) 
+    return redirect('home.index')
