0885edc48bf6294f3029d451900b44922823c90a
Ritwic <ritwic.verma.sachan@gmail.com>
2025-02-10 20:10:59 -0500

    added email to signup process

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
+from django.contrib.auth.models import User
+from django import forms
 
 class CustomErrorList(ErrorList):
     def __str__(self):
@@ -10,12 +12,22 @@ class CustomErrorList(ErrorList):
             f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self]))
 
 
-class CustomUserCreationForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        for fieldname in ['username', 'password1',
-        'password2']:
-            self.fields[fieldname].help_text = None
-            self.fields[fieldname].widget.attrs.update(
-                {'class': 'form-control'}
-            )
+class CustomUserCreationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ('username', 'email','password')
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -10,18 +10,18 @@ def sign_up(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
 
-    if request.method == 'GET':
-        template_data['form'] = CustomUserCreationForm()
-        return render(request, 'accounts/signup.html', {"template_data": template_data})
+    # if request.method == 'GET':
+    #     template_data['form'] = CustomUserCreationForm()
+    #     return render(request, 'accounts/signup.html', {"template_data": template_data})
 
-    elif request.method == 'POST':
+    if request.method == 'POST':
         form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
             return redirect('accounts.login')
-        else:
-            template_data['form'] = form
-            return render(request, 'accounts/signup.html', {"template_data": template_data})
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/signup.html', {"form": form})
 
 
 def login(request):
